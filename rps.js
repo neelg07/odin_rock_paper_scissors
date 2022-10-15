48ec890f62cf4c24142b84e72dc4507560c193fb
neelg07 <ineel7762@gmail.com>
2022-10-14 21:33:45 -0400

    Add three vital game functions in JS

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 console.log('Hello World')
 
 // Func that returns computer's choice
-let pc_choice = () => {
+function getComputerChoice() {
 
     const choices = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * choices.length);
@@ -9,4 +9,29 @@ let pc_choice = () => {
 
 }
 
-console.log(pc_choice());
+console.log(getComputerChoice());
+
+// Func to play one round, taking in player and AI selection as arguments
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === computerSelection) {                                        // Condition if tie game
+        return `It's a tie! Both players selected ${playerSelection}`;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {           // Conditions if player loses to computer
+        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {        // Conditions if player wins against computer
+        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+    } else {
+        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+    }
+}
+
+// Func to capitalize the first letter in string
+function title(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
