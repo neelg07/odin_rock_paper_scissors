7d0b4f8a181ecb9457721352fe8d7791be4add4e
neelg07 <ineel7762@gmail.com>
2022-10-14 21:37:10 -0400

    Add title function to all conditional in playRound

--- rps.js
+++ rps.js
@@ -15,19 +15,19 @@ console.log(getComputerChoice());
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === computerSelection) {                                        // Condition if tie game
-        return `It's a tie! Both players selected ${playerSelection}`;
+        return `It's a tie! Both players selected ${title(playerSelection)}!`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {           // Conditions if player loses to computer
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        return `You Lose! ${title(computerSelection)} beats ${title(playerSelection)}!`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        return `You Lose! ${title(computerSelection)} beats ${title(playerSelection)}!`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+        return `You Lose! ${title(computerSelection)} beats ${title(playerSelection)}!`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {        // Conditions if player wins against computer
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        return `You Win! ${title(playerSelection)} beats ${title(computerSelection)}!`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        return `You Win! ${title(playerSelection)} beats ${title(computerSelection)}!`;
     } else {
-        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+        return `You Win! ${title(playerSelection)} beats ${title(computerSelection)}!`;
     }
 }
 
